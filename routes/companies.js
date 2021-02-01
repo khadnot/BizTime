20cfbf581a0e04a00fde84f07c5722b5f804441d
Kenn Hadnot <k_hadnot@yahoo.com>
2021-02-01 12:12:19 -0800

    added post and put routes

--- routes/companies.js
+++ routes/companies.js
@@ -9,9 +9,9 @@ router.get('/', async (req, res, next) => {
             `SELECT code, name 
              FROM companies`
         );
-        return res.json({ "companies": companies.rows });
+        return res.json({ 'companies': companies.rows });
     } catch {
-        const err = new ExpressError('Error with retrieving from database', 400);
+        const err = new ExpressError('Error retrieving from database', 400);
         return next(err)
     }
 });
@@ -26,15 +26,53 @@ router.get('/:code', async (req, res, next) => {
         let company = companyRes.rows[0];
 
         if (companyRes.rows.length === 0) {
-            const err = new ExpressError('Company Not Found', 400);
+            const err = new ExpressError('Company Not Found', 404);
             return next(err);
         }
 
         return res.json(company);
     } catch {
-        const err = new ExpressError('Company Not Found', 400);
-        return next(err)
+        const err = new ExpressError('Error retrieving from database', 400);
+        return next(err);
     }
 });
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        let newCompany = await db.query(
+            `INSERT INTO companies
+             VALUES ($1, $2, $3)
+             RETURNING code, name, description`, [code, name, description]
+        );
+        res.json({ 'company': newCompany.rows });
+    } catch {
+        const err = new ExpressError('Error adding company to database', 400);
+        return next(err);
+    }
+});
+
+router.put('/:code', async (req, res, next) => {
+    try {
+        const { name, description } = req.body;
+        let code = req.params.code;
+        let editCompany = await db.query(
+            `UPDATE companies
+             SET name = $1, description = $2
+             WHERE code = $3
+             RETURNING code, name, description`, [name, description, code]
+        );
+        if (editCompany.rows.length === 0) {
+            const err = new ExpressError('Company not found', 404);
+            return next(err);
+        };
+        res.json({ 'company': editCompany.rows })
+    } catch {
+        const err = new ExpressError('Error retrieving from database', 400);
+        return next(err);
+    }
+})
+
+
 
 module.exports = router;
